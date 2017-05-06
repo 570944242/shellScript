fa7aadfe6f3a4788eb111de7f65d88e62ad74787
Yuan Zhao <786211677@qq.com>
2017-05-06 15:39:41 +0800

    install nginx1.7

---
+++ nginx_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script to install on centos6.5
+echo "Instaling ..."
+yum insatll -y gcc
+yum install -y pcre pcre-devel
+yum install -y zlib zlib-devel
+yum install -y openssl openssl-devel
+clear
+# dowmload 
+mkdir nginx_env
+cd nginx_env
+wget http://http://nginx.org/download/nginx-1.13.0.tar.gz
+tar -xjvf nginx-1.3.0.tar.gz
+cd nginx-1.3.0
+./configure
+make
+make install
+clear
+echo "nginx defult path : " $(whereis nginx)
+
+# nginx start
+cd /usr/local/nginx/sbin
+./nginx
